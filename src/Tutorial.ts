82bc066adaf6710474d7b70c46540ef297f262e6
JOSEPH-Inalegwu <josephjonahinalegwu@gmail.com>
2025-06-11 20:07:11 +0100

    solved first challenge

---
+++ src/Tutorial.ts
@@ -0,0 +1,21 @@
+// CHALLENGE
+
+// 1 Create a variable of type string and try to invoke a string method on it.
+
+// 2 Create a variable of type number and try to perform a mathematical operation on it.
+
+// 3 Create a variable of type boolean and try to perform a logical operation on it.
+
+let username:string = 'Shaw Brian' 
+username = username.toUpperCase()
+
+let price:number = 20  
+price = price + 0.99
+
+let isInStock:boolean = price > 10
+
+
+console.log(username, price, isInStock);
+
+
+
