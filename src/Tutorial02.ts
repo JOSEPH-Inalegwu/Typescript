9089168bb36425ab83aabd1fcc8bfbf42cf9c809
JOSEPH-Inalegwu <josephjonahinalegwu@gmail.com>
2025-06-13 11:56:38 +0100

    Practiced object type annotations with readonly and optional properties in TypeScript

--- src/Tutorial02.ts
+++ src/Tutorial02.ts
@@ -10,7 +10,7 @@ let randoms: number[] = []
 let names = ['Peter', 'Susan', 15, true]
 let array: (string | boolean)[] = ['Apple', true, 'Mango', false]
 
-console.log(array);
+// console.log(array);
 
 
 // CHALLENGE
@@ -21,3 +21,43 @@ let colors: string[] = ['red', 'green', 'yellow']
 // colors.push(true)
 
 let mixedArray: (number | string)[] = [2, 'three', 10]
+
+
+// OBJECTS IN TYPESCRIPT
+let car: {brand:string; year:number} = {
+    brand: 'BMW',
+    year: 2025,
+    // color: 'blue' //Error
+}
+car.brand = 'Lamborghini'
+
+
+let book = {title:'book', cost:30}
+let pen = {title:'pen', cost:20}
+let notebook = {title:'notebook'}
+
+let items: {readonly title:string; cost?:number}[] = [book,pen,notebook]
+
+// items[0].title = 'Supacell' //Error
+console.log(items);
+
+// OBJECT CHALLENGE
+let bike: {brand:string; year:number} = {
+    brand: 'Suzuki',
+    year: 2023
+}
+// bike.year = 'new'
+
+let laptop: {brand:string; year:number} = {
+    brand: 'Mac',
+    year: 2023
+}
+// laptop.brand = true
+
+let product1 = {title:'shirt', price:30}
+let product2 = {title:'pants'}
+
+let products: {title:string; price?:number}[] = [
+    product1, product2
+]
+products.push({title:'shoes'})
