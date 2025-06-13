02b8bbd60d13b8791b3214c7eeb3dbcee4adb288
JOSEPH-Inalegwu <josephjonahinalegwu@gmail.com>
2025-06-13 11:26:30 +0100

    Practiced array type annotations and handled union types in arrays

---
+++ src/Tutorial02.ts
@@ -0,0 +1,23 @@
+// ARRAYS IN TYPESCRIPT
+let prices:number[] = [100, 200, 300, 400, 500];
+// prices.push('600');
+
+let fruits:string[] = ['apple', 'banana', 'orange'];
+
+// let randoms:[] = ['apple']
+let randoms: number[] = []
+
+let names = ['Peter', 'Susan', 15, true]
+let array: (string | boolean)[] = ['Apple', true, 'Mango', false]
+
+console.log(array);
+
+
+// CHALLENGE
+let temperatures: number[] = [20, 25, 31]
+// temperatures.push('hot')
+
+let colors: string[] = ['red', 'green', 'yellow']
+// colors.push(true)
+
+let mixedArray: (number | string)[] = [2, 'three', 10]
