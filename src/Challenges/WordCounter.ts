533cd6ccccca780e6d47fc2760bb6d221042c266
JOSEPH-Inalegwu <josephjonahinalegwu@gmail.com>
2025-06-18 14:03:48 +0100

    Implement word counting function to tally occurrences in an array

--- src/Challenges/WordCounter.ts
+++ src/Challenges/WordCounter.ts
@@ -1,1 +1,17 @@
-console.log('Hello');
+
+function countWords(words: string[]): object {
+    const result = {}
+
+    words.forEach((word) => {
+        if (word in result) {
+            result[word] += 1
+        } else {
+            result[word] = 1
+        }
+    });
+
+    return result
+}
+
+const result = countWords(["apple", "banana", "apple", "orange", "banana", "apple"])
+console.log(result);
